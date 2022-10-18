8c88000b978f2c8211629da1b5983d53362f4d22
AlfredoCM55 <joy.porta@gmail.com>
2022-10-17 23:10:08 -0500

    Terminado nuevas funcionalidades

--- script.js
+++ script.js
@@ -1,33 +1,143 @@
-    //alert("Bienvenido")
-    //const verbs['open']
-    const showVerb = document.getElementById('showVerb')
-    const showImage = document.getElementById('showImage')
-    const showAudio = document.getElementById('shoAudio')
+const showAction = document.getElementById("showAction");
+const showImage = document.getElementById("showImage");
+const showAudio = document.getElementById("showAudio");
+const next = document.getElementById("next");
+const verbsCounter = document.getElementById("verbs-counter");
+const allRightCounter = document.getElementById("all-right-answers");
+const verbsContainer = document.getElementById("verbs-container");
+const btn = document.querySelector("refresh");
+
+const first = document.getElementById("first-verb");
+const second = document.getElementById("second-verb");
+const third = document.getElementById("third-verb");
+const fourth = document.getElementById("fourth-verb");
 
 
-    const first = document.getElementById('first-verb')
-    const second = document.getElementById('second-verb')
-    const third = document.getElementById('third-verb')
-    const fourth = document.getElementById('fourth-verb')
+const numberOfVerbs = verbs.length;
+let answerRoullete = [0,1,1,1];
+let everyNumberOfVerbs = [];
 
-    const next = document.getElementById('next')
-    const counter = document.getElementById('counter')
-    const allrightAnswers = document.getElementById('all-right-answers')
-    const verbsContainer = document.getElementById('verbs-container')
+let rightAnswer; 
+let allRightAnswers = 0; 
+next.addEventListener("click",function(){
+  ponerVerbo();
+  next.style.display = 'none';
+});
+makeRandomList();
+
+let position = everyNumberOfVerbs.length-1;
+
+function makeRandomList(){
+  
+  for (var i = 0; i < numberOfVerbs; i++){
+    everyNumberOfVerbs.push(i);
+  }
+  everyNumberOfVerbs = everyNumberOfVerbs.sort(() => Math.random() - 0.5);
+}
+
+function buttonEffect(itsRight,button){
+  if (itsRight){
+    button.classList.add('rightAnswer');
+    setTimeout(function(){
+      button.classList.remove('rightAnswer');
+    },300);
+    allRightAnswers = allRightAnswers+1;
+  }else{
+    button.classList.add('wrongAnswer');
+    setTimeout(function(){
+      button.classList.remove('wrongAnswer');
+    },300);
+  }
+  setTimeout(function(){
+    ponerVerbo();
+  },300);
+}
+
+first.addEventListener("click",function(){
+  buttonEffect(itsRight_(first.innerHTML),this);
+});
+
+second.addEventListener("click", function(){
+  buttonEffect(itsRight_(second.innerHTML),this);
+});
+
+third.addEventListener("click", function(){
+  buttonEffect(itsRight_(third.innerHTML),this);
+});
+
+fourth.addEventListener("click", function(){
+  buttonEffect(itsRight_(fourth.innerHTML),this);
+});
+
+function shuffleAnswers(array) {
+  
+  let currentIndex = array.length;
+  
+  let randomIndex;
+
+  while (currentIndex != 0) {
+
    
-    const numberOfVerbs = verbs.length;
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
 
-    console.log(numberOfVerbs);
-
-    let answersRoullete = [0,1,1,1];
-    let everyNumberOfVerbs = [];
-    let allRightAnswers;
-
-    next.addEventListener('click', function(){
-        ponerVerbo();
-        next.style.display = 'none';
-    });
     
-    function ponerVerbo(){
-        showVerb.innerHTML = 'Deposita 500 bitcoins para jugar'
-    }
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function itsRight_(answer){
+  return answer==rightAnswer?true:false;
+}
+
+function randomVerbo(notThisOne){
+  theOne = Math.floor(Math.random()*verbos.length);
+
+  return theOne == notThisOne?randomVerbo(notThisOne):theOne;
+}
+
+function ponerVerbo(){
+
+  answerRoullete = shuffleAnswers(answerRoullete);
+
+  let randomPosition = everyNumberOfVerbs[position];
+  let imgText = "<img src='Imagenes/"+verbs[randomPosition]+".jpg' height:'140px' width='100px'>";
+
+  first.classList.add("btn","btn-outline-light","btn-md");
+  second.classList.add("btn","btn-outline-light","btn-md");
+  third.classList.add("btn","btn-outline-light","btn-md");
+  fourth.classList.add("btn","btn-outline-light","btn-md");
+
+  if (position >= 0){
+    var just_position = position+1;
+    verbsCounter.innerHTML = ""+just_position+" / "+numberOfVerbs;
+    allRightCounter.innerHTML = "Right answers: "+allRightAnswers;
+    showAction.innerHTML = verbs[randomPosition];
+    showImage.innerHTML = imgText;
+
+    showAudio.src = "audios/"+verbs[randomPosition]+".mp3";
+    showAudio.play();
+
+    first.innerHTML = !answerRoullete[0]?verbos[randomPosition]:verbos[randomVerbo(randomPosition)];
+    second.innerHTML = !answerRoullete[1]?verbos[randomPosition]:verbos[randomVerbo(randomPosition)];
+    third.innerHTML = !answerRoullete[2]?verbos[randomPosition]:verbos[randomVerbo(randomPosition)];
+    fourth.innerHTML = !answerRoullete[3]?verbos[randomPosition]:verbos[randomVerbo(randomPosition)];
+
+    rightAnswer = verbos[randomPosition];
+    position = position - 1;
+  }else{
+    
+    verbsCounter.innerHTML = "0 / "+numberOfVerbs;
+    allRightCounter.innerHTML = "Right answers: "+allRightAnswers;
+    showAction.innerHTML = "Haz Acertado";
+    
+    btn.addEventListener("click", function(){
+        location.reload();
+        console.log("pagereload")
+    })
+    verbsContainer.innerHTML = "";
+  }
+}
